41ef0102aa901e64b28c1a1bc37b5b9e36600a69
sbayreder <sbayreder@gmail.com>
2020-05-15 18:44:13 -0500

    fixed links 2nd time

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 
 // Middleware
 //app.use(flash());
@@ -30,13 +28,12 @@ require("./routes/loginRoutes")(app);
 require("./routes/memberRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 // if (process.env.NODE_ENV === "test") {
-  syncOptions.force = false;
+syncOptions.force = false;
 // }
 
 // Starting the server, syncing our models ------------------------------------/
